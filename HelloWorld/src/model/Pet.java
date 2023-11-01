8f48a791cb7e1a542c5e547fd4577e38a31dcaf8
Josef Kudláček <149613366+josef-kudlacek@users.noreply.github.com>
2023-11-01 21:52:45 +0100

    Add files for OOP inheritance

---
+++ HelloWorld/src/model/Pet.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private String name;
+    private LocalDate dob;
+
+    public Pet() {
+    }
+
+    public Pet(String name, LocalDate dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    protected void protectedMethod() {
+        System.out.println("This is the protectedMethod of Pet");
+    }
+
+    void packageProtected() {
+        System.out.println("This is the packageProtected method of Pet");
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "name='" + name + '\'' +
+                ", dob=" + dob +
+                '}';
+    }
+}
---
+++ HelloWorld/src/specific/animals/Dog.java
@@ -0,0 +1,16 @@
+package specific.animals;
+
+import model.Pet;
+
+import java.time.LocalDate;
+
+public class Dog extends Pet {
+
+    public Dog(String name, LocalDate dob) {
+        super(name, dob);
+    }
+
+    public void bark() {
+        System.out.println("Ruff ruff!!!");
+    }
+}
