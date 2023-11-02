e1a85633955db3f87d06ac3dd21ebaa6061a2475
Josef Kudlacek <j.k.kudlacek@gmail.com>
2023-11-02 18:41:44 +0100

    Add employee without role.

--- Employees/src/main/java/eu/kudljo/emloyees/Main.java
+++ Employees/src/main/java/eu/kudljo/emloyees/Main.java
@@ -8,17 +8,18 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         String people = """
-            Flintstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
-            Flintstone2, Fred, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
-            Flintstone3, Fred, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
-            Flintstone4, Fred, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
-            Flintstone5, Fred, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
-            Rubble, Barney, 2/2/1905, Manager, {orgSize=300,dr=10}
-            Rubble2, Barney, 2/2/1905, Manager, {orgSize=100,dr=4}
-            Rubble3, Barney, 2/2/1905, Manager, {orgSize=200,dr=2}
-            Rubble4, Barney, 2/2/1905, Manager, {orgSize=500,dr=8}
-            Rubble5, Barney, 2/2/1905, Manager, {orgSize=175,dr=20}
-            Flintstone, Wilma, 3/3/1910, Analyst, {projectCount=3}
+                Flintstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
+                Flintstone, Fred, 1/1/1900, Programmerzz, {locpd=2000,yoe=10,iq=140}
+                Flintstone2, Fred, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
+                Flintstone3, Fred, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
+                Flintstone4, Fred, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
+                Flintstone5, Fred, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
+                Rubble, Barney, 2/2/1905, Manager, {orgSize=300,dr=10}
+                Rubble2, Barney, 2/2/1905, Manager, {orgSize=100,dr=4}
+                Rubble3, Barney, 2/2/1905, Manager, {orgSize=200,dr=2}
+                Rubble4, Barney, 2/2/1905, Manager, {orgSize=500,dr=8}
+                Rubble5, Barney, 2/2/1905, Manager, {orgSize=175,dr=20}
+                Flintstone, Wilma, 3/3/1910, Analyst, {projectCount=3}
             Flintstone2, Wilma, 3/3/1910, Analyst, {projectCount=4}
             Flintstone3, Wilma, 3/3/1910, Analyst, {projectCount=5}
             Flintstone4, Wilma, 3/3/1910, Analyst, {projectCount=6}
