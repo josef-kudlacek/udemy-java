f301955a8734bb766db2d344045acf09dc62a115
Josef Kudlacek <j.k.kudlacek@gmail.com>
2023-11-05 12:22:25 +0100

    143. LinkedList and concurrent exception.

--- Employees/src/main/java/eu/kudljo/emloyees/Main.java
+++ Employees/src/main/java/eu/kudljo/emloyees/Main.java
@@ -3,6 +3,7 @@ package eu.kudljo.emloyees;
 import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
@@ -12,20 +13,20 @@ public class Main {
         String people = """
                 Flintstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
                 Flintstone, Fred, 1/1/1900, Programmerzz, {locpd=2000,yoe=10,iq=140}
-                Flintstone2, Fred, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
-                Flintstone3, Fred, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
-                Flintstone4, Fred, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
-                Flintstone5, Fred, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
+                Flintstone2, Fred2, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
+                Flintstone3, Fred3, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
+                Flintstone4, Fred4, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
+                Flintstone5, Fred5, 1/1/1900, Programmer, {locpd=5,yoe=10,iq=100}
                 Rubble, Barney, 2/2/1905, Manager, {orgSize=300,dr=10}
-                Rubble2, Barney, 2/2/1905, Manager, {orgSize=100,dr=4}
-                Rubble3, Barney, 2/2/1905, Manager, {orgSize=200,dr=2}
-                Rubble4, Barney, 2/2/1905, Manager, {orgSize=500,dr=8}
-                Rubble5, Barney, 2/2/1905, Manager, {orgSize=175,dr=20}
+                Rubble2, Barney2, 2/2/1905, Manager, {orgSize=100,dr=4}
+                Rubble3, Barney3, 2/2/1905, Manager, {orgSize=200,dr=2}
+                Rubble4, Barney4, 2/2/1905, Manager, {orgSize=500,dr=8}
+                Rubble5, Barney5, 2/2/1905, Manager, {orgSize=175,dr=20}
                 Flintstone, Wilma, 3/3/1910, Analyst, {projectCount=3}
-                Flintstone2, Wilma, 3/3/1910, Analyst, {projectCount=4}
-                Flintstone3, Wilma, 3/3/1910, Analyst, {projectCount=5}
-                Flintstone4, Wilma, 3/3/1910, Analyst, {projectCount=6}
-                Flintstone5, Wilma, 3/3/1910, Analyst, {projectCount=9}
+                Flintstone2, Wilma2, 3/3/1910, Analyst, {projectCount=4}
+                Flintstone3, Wilma3, 3/3/1910, Analyst, {projectCount=5}
+                Flintstone4, Wilma4, 3/3/1910, Analyst, {projectCount=6}
+                Flintstone5, Wilma5, 3/3/1910, Analyst, {projectCount=9}
                 Rubble, Betty, 4/4/1915, CEO, {avgStockPrice=300}
                 """;
 
@@ -37,7 +38,7 @@ public class Main {
         int totalSalaries = 0;
         // Prefer the most generic instance of object to use.
         IEmployee employee;
-        List<IEmployee> employeeList = new ArrayList<>();
+        List<IEmployee> employeeList = new LinkedList<>();
         while (peopleMatcher.find()) {
             employee = Employee.createEmployee(peopleMatcher.group());
             employeeList.add(employee);
@@ -65,7 +66,20 @@ public class Main {
 //            totalSalaries += employee.getSalary();
         }
 
+        List<String> removalFirstNames = new ArrayList<>();
+        removalFirstNames.add("Wilma5");
+        removalFirstNames.add("Barney4");
+        removalFirstNames.add("Fred2");
+
         for (IEmployee employeeElement : employeeList) {
+            // Check class including subclasses
+            if (employeeElement instanceof Employee) {
+                Employee tempEmployee = (Employee) employeeElement;
+
+                if (removalFirstNames.contains(tempEmployee.getFirstName())) {
+                    employeeList.remove(employeeElement);
+                }
+            }
             System.out.println(employeeElement.toString());
             totalSalaries += employeeElement.getSalary();
         }
