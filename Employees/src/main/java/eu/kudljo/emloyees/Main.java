0aa6fae3c684ac4b58db5b20ff361bbb5fe61bbe
Josef Kudlacek <j.k.kudlacek@gmail.com>
2023-11-09 17:04:28 +0100

    155. Map - using map for search of element by key (firstName).

--- Employees/src/main/java/eu/kudljo/emloyees/Main.java
+++ Employees/src/main/java/eu/kudljo/emloyees/Main.java
@@ -1,21 +1,23 @@
 package eu.kudljo.emloyees;
 
 import java.text.NumberFormat;
-import java.util.Locale;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.regex.Matcher;
 
 public class Main {
+
+    private static Set<IEmployee> employees;
+    private static Map<String, Employee> employeeMap;
+
     public static void main(String[] args) {
         String people = """
                 Flintstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
-                Flintstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
-                Flintstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
-                Flintstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
-                Flintstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
-                Flintstone, Fred, 1/1/1900, Programmer, {locpd=2000,yoe=10,iq=140}
-                Flintstone, Fred, 1/1/1900, Programmerzz, {locpd=2000,yoe=10,iq=140}
+                Flintstone, Fred, 1/1/1900, Programmer, {locpd=4000,yoe=10,iq=140}
+                Flintstone, Fred, 1/1/1900, Programmer, {locpd=5000,yoe=10,iq=140}
+                Flintstone, Fred, 1/1/1900, Programmer, {locpd=6000,yoe=10,iq=140}
+                Flintstone, Fred, 1/1/1900, Programmer, {locpd=7000,yoe=10,iq=140}
+                Flintstone, Fred, 1/1/1900, Programmer, {locpd=9000,yoe=10,iq=140}
+                Flintstone, Fred, 1/1/1900, Programmerzz, {locpd=9000,yoe=10,iq=140}
                 Flintstone2, Fred2, 1/1/1900, Programmer, {locpd=1300,yoe=14,iq=100}
                 Flintstone3, Fred3, 1/1/1900, Programmer, {locpd=2300,yoe=8,iq=105}
                 Flintstone4, Fred4, 1/1/1900, Programmer, {locpd=1630,yoe=3,iq=115}
@@ -38,19 +40,27 @@ public class Main {
         int totalSalaries = 0;
         // Prefer the most generic instance of object to use.
         IEmployee employee;
-        Set<IEmployee> employeeList = new TreeSet<>();
+        employees = new TreeSet<>();
+        employeeMap = new LinkedHashMap<>();
         while (peopleMatcher.find()) {
             employee = Employee.createEmployee(peopleMatcher.group());
-            employeeList.add(employee);
+            Employee emp = (Employee) employee;
+            employees.add(employee);
+            employeeMap.put(emp.firstName, emp);
         }
 
-        for (IEmployee employeeElement : employeeList) {
+        for (IEmployee employeeElement : employees) {
             System.out.println(employeeElement.toString());
             totalSalaries += employeeElement.getSalary();
         }
 
         NumberFormat currencyInstance = NumberFormat.getCurrencyInstance(Locale.US);
         System.out.printf("The total payout should be %s%n", currencyInstance.format(totalSalaries));
-        System.out.println(employeeList.size());
+        System.out.println(employees.size());
+        System.out.println(employeeMap);
+    }
+
+    public int getSalary(String firstName) {
+        return employeeMap.get(firstName).getSalary();
     }
 }
