a3ab0486f391021b3b8ae807f7a8fdde9edfe223
Josef Kudlacek <j.k.kudlacek@gmail.com>
2024-01-24 22:29:13 +0100

    177. Stream - map reduce.

--- Employees/src/main/java/eu/kudljo/emloyees/Streams.java
+++ Employees/src/main/java/eu/kudljo/emloyees/Streams.java
@@ -1,10 +1,10 @@
 package eu.kudljo.emloyees;
 
-import java.text.NumberFormat;
-import java.util.Locale;
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
 
@@ -35,86 +35,30 @@ public class Streams {
                 Rubble, Betty, 4/4/1915, CEO, {avgStockPrice=300}
                 """;
 
-//        Predicate<Employee> dummyEmployeePredicate = employee -> "N/A".equals(employee.getLastName());
-        Optional<Employee> optionalEmployee = peopleText.lines()
-                .map(Employee::createEmployee)
-                .map(employee -> (Employee) employee)
-                .filter(employee -> employee.getSalary() < 0)
-//                .noneMatch(employee -> employee.getSalary() < 0);
-                .findFirst();
 
-        System.out.println(optionalEmployee
-                .map(Employee::getFirstName)
-                .orElse("Nobody"));
-
-        System.out.println();
-//        try {
-//            Files.lines(Path.of("C:\\Users\\Pepa Kudláček\\IdeaProjects\\udemy-java\\Employees\\src\\main\\java\\eu\\kudljo\\emloyees\\employees.txt"))
-//                    .forEach(System.out::println);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-
-//        Predicate<String> dummyEmployeeSelector = s -> s.contains("Programmerzz");
         Predicate<Employee> employeePredicate = employee -> "N/A".equals(employee.getLastName());
         Predicate<Employee> underFiveKSelector = employee -> employee.getSalary() < 5000;
         Predicate<Employee> noDummiesAndUnderFiveK = employeePredicate.negate().and(underFiveKSelector);
-        int sum = peopleText.lines()
-//                .filter(not(s -> s.contains("Programmerzz")))
-//                .filter(dummyEmployeeSelector.negate())
+
+        OptionalInt result = peopleText.lines()
                 .map(Employee::createEmployee)
                 .map(employee -> (Employee) employee)
                 .filter(noDummiesAndUnderFiveK)
-                // usage of Predicate with NOT
-//                .filter(not(employee -> employee instanceof Programmer))
-//                .filter(not(employee -> "N/A".equals(employee.getLastName())))
-//                .filter(employee -> employee.getSalary() > 5000)
-//                .filter(employee -> employee.getSalary() < 10000)
-                // remove duplicates by input them to Set
                 .collect(Collectors.toSet()).stream()
-                // nested sorting by multiple attributes
                 .sorted(comparing(Employee::getLastName)
                         .thenComparing(Employee::getFirstName)
                         .thenComparingInt(Employee::getSalary)
                 )
                 .mapToInt(Streams::showEmployeeAndGetSalary)
-                .sum();
+                .reduce((a, b) -> Math.max(a, b));
 
-        System.out.printf("%nSum of salaries is: %s", NumberFormat.getCurrencyInstance(Locale.US)
-                .format(sum));
+        System.out.println(result.orElse(0));
 
+        Optional<String> output = Stream.of("tom", "jerry", "mary", "sam")
+                .reduce((a, b) -> a.toUpperCase().concat("_").concat(b.toUpperCase()));
 
-//        Collection<String> nums = Set.of("one", "two", "three", "four");
-//        nums
-//                .stream()
-//                .map(String::hashCode)
-//                .map(Integer::toHexString)
-//                .forEach(System.out::println);
-//
-//        record Car(String make, String model){}
-//
-//        Stream.of(new Car("Ford", "Bronco"), new Car("Tesla", "X"), new Car("Tesla", "3"))
-//                .filter(car -> "Tesla".equals(car.make))
-//                .forEach(System.out::println);
-
-        // Filter null values in stream
-//        String myVar = null;
-//        Stream myStream = null;
-//
-//        Stream.ofNullable(myVar)
-//                .forEach(System.out::println);
-
-        // Stream especially for integers
-//        IntStream.rangeClosed(1, 40)
-//                .mapToObj(String::valueOf)
-//                .map(s -> s.concat("-"))
-//                .forEach(System.out::print);
-
-//        String[] names = {"terry", "sam", "jake"};
-//        Arrays.stream(names)
-//                .map(String::toUpperCase)
-//                .forEach(System.out::println);
+        // tom_jerry_mary_sam
+        System.out.println(output.orElse("N/A"));
     }
 
     public static int showEmployeeAndGetSalary(IEmployee employee) {
