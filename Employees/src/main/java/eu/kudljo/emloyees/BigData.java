2ef8117dda5447731bb95da056d4ed27e62bd96d
Josef Kudlacek <j.k.kudlacek@gmail.com>
2024-01-28 10:40:25 +0100

    179. Stream - big data summing

--- Employees/src/main/java/eu/kudljo/emloyees/BigData.java
+++ Employees/src/main/java/eu/kudljo/emloyees/BigData.java
@@ -3,16 +3,25 @@ package eu.kudljo.emloyees;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 public class BigData {
     public static void main(String[] args) {
         try {
-            List<String> result = Files.lines(Path.of("PATH_OF_FILE"))
-                    .skip(1)
-                    .toList();
+            long startTime = System.currentTimeMillis();
 
-//            System.out.println(result);
+            Long result = Files.lines(Path.of("PATH_OF_FILE")).parallel()
+                    .skip(1)
+//                    .limit(10)
+                    .map(line -> line.split(","))
+                    .map(line -> line[25])
+                    .mapToLong(salary -> Long.parseLong(salary))
+                    .sum();
+//                    .collect(Collectors.summingLong(salary -> Long.parseLong(salary)));
+
+            long endTime = System.currentTimeMillis();
+
+            System.out.printf("$%,d.00%n", result);
+            System.out.println(endTime - startTime);
         } catch (IOException e) {
             e.printStackTrace();
         }
