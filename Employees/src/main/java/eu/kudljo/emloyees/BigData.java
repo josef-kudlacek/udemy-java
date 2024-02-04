acc1e913012e87e937f64ad7bf1e52606886a051
Josef Kudlacek <j.k.kudlacek@gmail.com>
2024-02-04 21:02:12 +0100

    181. Stream - grouping records

--- Employees/src/main/java/eu/kudljo/emloyees/BigData.java
+++ Employees/src/main/java/eu/kudljo/emloyees/BigData.java
@@ -3,8 +3,12 @@ package eu.kudljo.emloyees;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-import static java.util.stream.Collectors.summingLong;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 public class BigData {
 
@@ -15,16 +19,17 @@ public class BigData {
         try {
             long startTime = System.currentTimeMillis();
 
-            Long result = Files.lines(Path.of("PATH_OF_FILE")).parallel()
+            Map<String, List<Person>> result = Files.lines(Path.of("PATH_OF_FILE")).parallel()
                     .skip(1)
-//                    .limit(10)
+                    .limit(10)
                     .map(line -> line.split(","))
                     .map(array -> new Person(array[2], array[4], Long.parseLong(array[25]), array[32]))
-                    .collect(summingLong(Person::salary));
+                    .collect(groupingBy(Person::state, TreeMap::new, toList()));
 
             long endTime = System.currentTimeMillis();
 
-            System.out.printf("$%,d.00%n", result);
+//            System.out.printf("$%,d.00%n", result);
+            System.out.println(result);
             System.out.println(endTime - startTime);
         } catch (IOException e) {
             e.printStackTrace();
