be404d4d4e8e5e9437fe35fdfd447a31ca300ce2
Josef Kudlacek <j.k.kudlacek@gmail.com>
2024-04-01 19:43:48 +0200

    185. Stream - partitioning and grouping

--- Employees/src/main/java/eu/kudljo/emloyees/BigData.java
+++ Employees/src/main/java/eu/kudljo/emloyees/BigData.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.text.NumberFormat;
-import java.util.Locale;
 import java.util.Map;
-import java.util.TreeMap;
 
 import static eu.kudljo.constants.FilePath.PATH_OF_EMPLOYEES_FILE;
 import static java.util.stream.Collectors.*;
@@ -23,19 +20,14 @@ public class BigData {
 
 //            Map<String, String> result =
 
-            TreeMap<String, Map<Character, String>> result = Files.lines(Path.of(PATH_OF_EMPLOYEES_FILE)).parallel()
+            Map<Boolean, Map<String, Long>> result = Files.lines(Path.of(PATH_OF_EMPLOYEES_FILE)).parallel()
                     .skip(1)
-//                    .limit(10)
+//                    .limit(200)
                     .map(line -> line.split(","))
                     .map(array -> new Person(array[2], array[4], new BigDecimal(array[25]), array[32], array[5].strip().charAt(0)))
-                    .collect(
-                            groupingBy(Person::state, TreeMap::new,
-                                    groupingBy(Person::gender,
-                                            collectingAndThen(
-                                                    reducing(BigDecimal.ZERO, Person::salary, (a, b) -> a.add(b)),
-                                                    NumberFormat.getCurrencyInstance(Locale.US)::format))
-                            ));
-//                    .forEach((state, salary) -> System.out.printf("%s \t %c \t %s%n", state, salary));
+//                    .forEach((state, salary) -> System.out.printf("%s -> %s%n", state, salary));
+                    .collect(partitioningBy(person -> person.gender() == 'F',
+                            groupingBy(Person::state, counting())));
 
             long endTime = System.currentTimeMillis();
 
